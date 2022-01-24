fae7527ba8beed64089062904b6f5cf5e10cd11a
rkdwlss2 <rkdwlss2@gmail.com>
2022-01-24 23:51:03 +0900

    Initial Commit

---
+++ codes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  codes
+//
+//  Created by 강명진 on 2022/01/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
