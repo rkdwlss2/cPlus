d6ba7cd860d0775f313dfba6975e74cb76164e85
rkdwlss2 <rkdwlss2@gmail.com>
2022-01-24 23:52:43 +0900

    firstUpdate

--- codes/main.cpp
+++ codes/main.cpp
@@ -4,11 +4,35 @@
 //
 //  Created by 강명진 on 2022/01/24.
 //
+//
+//  boj2386.cpp
+//  boj1926bfs
+//
+//  Created by 강명진 on 2022/01/24.
+//
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    while (true){
+        char tmp;
+        cin>>tmp;
+        if (tmp=='#')break;
+        int cnt=0;
+        string str;
+        getline(cin,str);
+        int intTmp=tmp-'a';
+        for (int i = 0 ; i<str.size();i++){
+            char strTmp=str[i];
+            int bigInt=strTmp-'A';
+            int smallInt=strTmp-'a';
+            if (bigInt==intTmp||smallInt==intTmp)cnt++;
+        }
+        cout<<tmp<<' '<<cnt<<'\n';
+    }
 }
+
